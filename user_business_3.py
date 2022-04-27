aec524db75c28243f02b4fe5c5bf596a283d170f
MTylerMartin <mtylermartin@gmail.com>
2022-04-26 21:45:51 -0500

    Calling add_user function and passing data into function to insert a new row into the Users table.  Selecting all from Users tables to check.

--- user_business_3.py
+++ user_business_3.py
@@ -16,11 +16,21 @@ def add_business(business_id, business_name, business_type, number_of_employees)
                 "VALUES (%s, %s, %s, %s)")
     mycursor.execute(b_insert, b_data)
 
-add_business(801, 'J Gilberts', 'Food', 44)
+#add_business(801, 'J Gilberts', 'Food', 44)
+
+def add_user(first_name, last_name, salary, sex, user_id, works_for):
+    u_data = (first_name, last_name, salary, sex, user_id, works_for)
+    u_insert = ("INSERT INTO Users (First_name, Last_name, Salary, Sex, User_ID, Works_For)"
+                "VALUES (%s, %s, %s, %s, %s, %s)")
+    mycursor.execute(u_insert, u_data)
 
 
+add_user('Travis', 'Barker', 3100000, 'M', 14, 501)
 
-mycursor.execute('SELECT * FROM Business')
+db.commit()
+
+#mycursor.execute('SELECT * FROM Business')
+mycursor.execute('SELECT * FROM Users')
 
 for x in mycursor:
     print(x)
