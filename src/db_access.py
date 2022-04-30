8cb028ad1dd31172bb2ea0f176cc3c2a80589d2a
MTylerMartin <mtylermartin@gmail.com>
2022-04-30 11:21:56 -0500

    Created objects for a New Business and New User from classes in db_access.  Created functions to connect to database and insert new business/user within the function.  Passed the database and new business/user objects into the functions.  Will work on creating objects from JSON file to be used with these classes/functions

--- src/db_access.py
+++ src/db_access.py
@@ -1,20 +1,29 @@
-import mysql.connector
+class DBAccess:
 
-db = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="Ng13gHc##",
-    database="user_business"
-)
-mycursor = db.cursor()
-
-class db_connector:
-
-    def __init__(self, business_id, business_name, busines_type, number_of_employees):
+    def __init__(self, host, user, password, database):
         self.host = host
         self.user = user
         self.password = password
         self.database = database
 
+
+class Business:
+
+    def __init__(self, business_id, business_name, business_type, number_of_employees):
+        self.business_id = int(business_id),
+        self.business_name = business_name,
+        self.business_type = business_type,
+        self.number_of_employees = int(number_of_employees)
+
+
+class User:
+
+    def __init__(self, first_name, last_name, salary, sex, user_id, works_for):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.salary = salary
+        self.sex = sex
+        self.user_id = user_id
+        self.works_for = works_for
 
 
