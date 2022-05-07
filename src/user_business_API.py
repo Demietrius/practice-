4160c84c5d5df828a5d89d8960665fdf4ba53f63
MTylerMartin <mtylermartin@gmail.com>
2022-05-07 15:37:03 -0500

    create and update

---
+++ src/user_business_API.py
@@ -0,0 +1,42 @@
+from db_access import DatabaseAccessor
+import json
+
+with open("../src/db_login.json", "r") as f:
+    db_login = json.load(f)
+    host = db_login["login"]["host"]
+    user = db_login["login"]["user"]
+    password = db_login["login"]["password"]
+    database = db_login["login"]["database"]
+    database = DatabaseAccessor(host, user, password, database)
+
+
+def create_handler(request):
+    if "user" not in request["request"]:
+        return 2, "error: no data for user"
+    database.create(request["request"]["user"])
+
+
+def update_handler(request):
+    if "user" not in request["request"]:
+        return 2, "error: no data for user"
+    database.update(request["request"]["user"])
+
+
+def handler(request):
+    if "commonParams" not in request:
+        return 2, " error: missing common parameter (commonParams)"
+
+    if "domain" in request["commonParams"] \
+            and "action" in request["commonParams"] \
+            and request["commonParams"]["domain"] == "userProfile":
+
+        if request["commonParams"]["action"] == "create":
+            create_handler(request)
+        if request["commonParams"]["action"] == "update":
+            update_handler(request)
+        if request["commonParams"]["action"] == "read":
+            # read_handler(request)
+            pass
+        if request["commonParams"]["action"] == "delete":
+            # delete_handler(request)
+            pass
